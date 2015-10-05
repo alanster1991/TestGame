35fee07e27d3420f0d20eff8610f939b7950c50b
Song Zheng <song@tokbox.com>
2015-10-05 06:13:57 -0700

    fix view problem

--- view.js
+++ view.js
@@ -2,20 +2,27 @@ function moveView() {
   var moveLeft = 0;
   var img1 = $('#view');
   var img2 = $('#view2');
+  var swap = true;
   window.setInterval(function() {
-    moveLeft += 30;
+    moveLeft -= 50;
     var width = 1763;
     if( !img1 || !img2) {
       img1 = $('#view');
       img2 = $('#view2');
     }
-    img1.css("right", moveLeft);
-    img2.css("right", moveLeft - width);
-    if(moveLeft > width) {
+    img1.css("left", moveLeft);
+    img2.css("left", width + moveLeft);
+    if(moveLeft < width*-1) {
+      if(swap){
         img1 = $('#view2');
         img2 = $('#view');
-        moveLeft = 0;
+      }else{
+        img1 = $('#view');
+        img2 = $('#view2');
       }
+      swap = !swap;
+      moveLeft += width;
+    }
    }, 100);
    $(document).keydown(function(e) {
      if( e.keyCode == 32) {
