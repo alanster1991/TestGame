674590a8b3ce433b5e3eb4820bdad53a7c5af887
Alan Chin <willian.tun@gmail.com>
2015-10-04 21:08:38 -0700

    Initial commit

---
+++ view.js
@@ -0,0 +1,34 @@
+function moveView() {
+  var moveLeft = 0;
+  var img1 = $('#view');
+  var img2 = $('#view2');
+  window.setInterval(function() {
+    moveLeft += 30;
+    var width = 1763;
+    if( !img1 || !img2) {
+      img1 = $('#view');
+      img2 = $('#view2');
+    }
+    img1.css("right", moveLeft);
+    img2.css("right", moveLeft - width);
+    if(moveLeft > width) {
+        img1 = $('#view2');
+        img2 = $('#view');
+        moveLeft = 0;
+      }
+   }, 100);
+   $(document).keydown(function(e) {
+     console.log("KEYDOWN");
+     if( e.keyCode == 32) {
+       $('.person').css("top", 100);
+     
+    }
+   });
+   $(document).keyup(function(e) {
+    console.log("KEYUP");
+    if( e.keyCode == 32) {
+      $('.person').css("top", 200);
+    }
+   });
+
+}
